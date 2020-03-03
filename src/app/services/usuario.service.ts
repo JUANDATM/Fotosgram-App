37c4577e769fb7b067bf448eb0508131821ba95c
JUANDATM <kira00711@hotmail.com>
2020-03-03 17:13:09 -0600

    token storage Ok

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,32 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Storage } from '@ionic/storage';
 import { Usuario } from '../pages/interfaces/interfaces';
+import { NavController } from '@ionic/angular';
 
 const URL = environment.url;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UsuarioService {
   token: string = null;
+  usuario: Usuario = {};
 
-  constructor(private http: HttpClient,
-              private storage: Storage) { }
-
-
-
+  constructor(
+    private http: HttpClient,
+    private storage: Storage,
+    private navCtrl: NavController
+  ) {}
 
   login(email: string, password: string) {
-    const data = {email, password};
+    const data = { email, password };
     return new Promise(resolve => {
       this.http.post(`${URL}/user/login`, data).subscribe(resp => {
         console.log(resp);
-        if (resp['ok']) {
-          this.guardarToken(resp['token']);
+        if (resp["ok"]) {
+          this.guardarToken(resp["token"]);
           resolve(true);
-        } else{
+        } else {
           this.token = null;
           this.storage.clear();
           resolve(false);
@@ -35,28 +37,51 @@ export class UsuarioService {
     });
   }
 
-
-
   registro(usuario: Usuario) {
-
-    return new Promise(resolve =>{
-      this.http.post(`${URL}/user/create`, usuario).subscribe(resp =>{
-        if (resp['ok']) {
-          this.guardarToken(resp['token']);
+    return new Promise(resolve => {
+      this.http.post(`${URL}/user/create`, usuario).subscribe(resp => {
+        if (resp["ok"]) {
+          this.guardarToken(resp["token"]);
           resolve(true);
-        } else{
+        } else {
           this.token = null;
           this.storage.clear();
           resolve(false);
         }
-      })
+      });
     });
   }
 
-  async guardarToken(token: string){
+  async guardarToken(token: string) {
     this.token = token;
-    await this.storage.set('token', token);
+    await this.storage.set("token", token);
   }
 
+  async cargarToken() {
+    this.token = (await this.storage.get("token")) || null;
+  }
 
+  async validaToken(): Promise<boolean> {
+    await this.cargarToken();
+
+    if (!this.token) {
+      this.navCtrl.navigateRoot('/login');
+      return Promise.resolve(false);
+    }
+
+    return new Promise<boolean>(resolve => {
+      const headers = new HttpHeaders({
+        "x-token": this.token
+      });
+      this.http.get(`${URL}/user/`, { headers }).subscribe(resp => {
+        if (resp["ok"]) {
+          this.usuario = resp["usuario"];
+          resolve(true);
+        } else {
+          this.navCtrl.navigateRoot('/login');
+          resolve(false);
+        }
+      });
+    });
+  }
 }
